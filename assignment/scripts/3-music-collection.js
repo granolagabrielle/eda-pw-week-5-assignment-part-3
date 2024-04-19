8d7b3c80c199340ef2db487c1df0541b4ba5eee2
Gabrielle Lommel <granolagabrielle@gmail.com>
2024-04-19 12:48:26 -0500

    Update showCollection function to take in a collection parameter

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,8 +1,10 @@
 console.log('***** Music Collection *****')
 // Safe Zone -- Write code below this line
 
+// set myCollection to an empty array
 const myCollection = [];
 
+// write function to add album to myCollection array and return album once pushed
 function addToCollection(collection, title, artist, yearPublished) {
   const album = {
     collection,
@@ -14,6 +16,7 @@ function addToCollection(collection, title, artist, yearPublished) {
   return album;
 }
 
+// add 6 albums to myCollection
 console.log(addToCollection(myCollection, 'A Sea of Split Peas', 'Courtney Barnett', 2013));
 console.log(addToCollection(myCollection, 'Tell Me How You Really Feel', 'Courtney Barnett', 2018));
 console.log(addToCollection(myCollection, 'Free Love', 'Sylvan Esso', 2020));
@@ -21,18 +24,21 @@ console.log(addToCollection(myCollection, 'Jump Rope Gazers', 'The Beths', 2020)
 console.log(addToCollection(myCollection, 'Rumours', 'Fleetwood Mac', 1977));
 console.log(addToCollection(myCollection, 'Can\'t Buy A Thrill', 'Steely Dan', 1972));
 
-console.log(myCollection);
-
-function showCollection(album) {
+// write function to show collection
+function showCollection(collection, album) {
   for (album of myCollection) {
-    console.log(`${album.title} by ${album.artist}, published in ${album.yearPublished}`);
+    console.log(`${collection} ${album.title} by ${album.artist}, published in ${album.yearPublished}.`);
   }
 }
-// test showCollection
-showCollection('Album in my collection:');
 
-// function findByArtist (collection, artist) {}
+// test showCollection
+showCollection(`Album in my collection:`);
+
+// function to find albums by artist and add them to an array
+
+
 // test findByArtist
+// findByArtist('Courtney Barnett');
 
 // STRETCH GOALS
 
